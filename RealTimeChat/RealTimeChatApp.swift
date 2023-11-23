b77794df690287f66d039ae1284006313dd098a6
levanduy093-work <levanduy.work@gmail.com>
2023-11-23 22:34:13 +0700

    Initial Commit

---
+++ RealTimeChat/RealTimeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeChatApp.swift
+//  RealTimeChat
+//
+//  Created by Lê Văn Duy on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
