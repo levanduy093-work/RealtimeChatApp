75801d00b7a9372a84a816fdf6154aeba4a344b5
levanduy093-work <levanduy.work@gmail.com>
2023-11-24 12:09:23 +0700

    text&view+extension

--- RealTimeChat/Extensions/TextFieldModifier.swift
+++ RealTimeChat/Extensions/TextFieldModifier.swift
@@ -5,4 +5,15 @@
 //  Created by Lê Văn Duy on 23/11/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct TextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .cornerRadius(20)
+            .padding(.horizontal, 24)
+    }
+}
--- RealTimeChat/Extensions/View+Extension.swift
+++ RealTimeChat/Extensions/View+Extension.swift
@@ -5,4 +5,17 @@
 //  Created by Lê Văn Duy on 23/11/2023.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    @ViewBuilder
+    func hSpacing(_ alignment: Alignment) -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: alignment)
+    }
+    
+    func vSpacing(_ alignment: Alignment) -> some View {
+        self
+            .frame(maxHeight: .infinity, alignment: alignment)
+    }
+}
